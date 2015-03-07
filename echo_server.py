654c92a5a312bde7f210bb5d1ac90f8e5c0c1602
Edward <edpark13@gmail.com>
2015-03-07 14:05:20 -0800

    add resolve_uri and update response_ok and server with tests

--- echo_server.py
+++ echo_server.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
 import socket
+import os
+import mimetypes
 
-def response_ok():
-    return u"HTTP/1.1 200 OK\r\n \
-    Content-Type: text/plain\r\n\r\n".encode('utf-8')
+def response_ok(t):
+    okay = u"HTTP/1.1 200 OK\r\n"
+    uritype = u"Content-Type: " + t[0] + "\r\n"
+    c_len = u"Content-length: " + str(len(t[1]))
+    crlf = u"\r\n\r\n"
+    body = t[1]
+    return "{}{}{}{}{}".format(okay, uritype, c_len, crlf, body).encode('utf-8')
 
 def response_error(error_code, reason):
     return u"HTTP/1.1 {} {}\r\n \
@@ -22,6 +28,24 @@ def parse_request(request):
         return response_error('505', 'HTTP Version Not Supported')
     else:
         return uri
+
+def resolve_uri(uri):
+    home = os.path.join(os.getcwd(), 'webroot')
+    path = os.path.join(home, uri.lstrip("/"))
+    if os.path.isdir(path):
+        path_list = os.listdir(path)
+        dirs = ""
+        for i in path_list:
+            dirs += "<li>"+i+"</li>\n"
+        body = "<ul>\n{}</ul>\n".format(dirs)
+        uritype = 'text/html'
+    elif os.path.isfile(path):
+        f = open(path, 'r')
+        body = f.read()
+        uritype = mimetypes.guess_type(path)[0]
+    else:
+        return response_error('404', 'Not Found')
+    return uritype , body
 
 def server():
     server_socket = socket.socket(
@@ -45,7 +69,8 @@ def server():
             first_line = first_line.split(' ')
             uri = first_line[1]
             if str(uri) == str(parse_request(message)):
-                response = "{}{}".format(response_ok(), parse_request(message))
+                t = resolve_uri(uri)
+                response = "{}{}".format(response_ok(t), parse_request(message))
             else:
                 response = parse_request(message)
             conn.sendall(response)
